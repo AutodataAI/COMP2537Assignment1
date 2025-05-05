e5624ccc4a6e652c05c5e66372175d04cd076ff5
Ted Ip   <hip7@my.bcit.ca>
2025-05-05 13:53:55 -0700

    databaseconnection.js changed

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,10 +1,25 @@
 require('dotenv').config();
+const { MongoClient } = require("mongodb");
 
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 
-const MongoClient = require("mongodb").MongoClient;
-const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true`;
-var database = new MongoClient(atlasURI, {});
-module.exports = {database};
+const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority`;
+
+let mongoClient;
+
+module.exports = {
+  database: {
+    db: (dbName) => {
+      if (!mongoClient) {
+        mongoClient = new MongoClient(atlasURI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+      }
+
+      return mongoClient.db(dbName);
+    },
+  },
+};
